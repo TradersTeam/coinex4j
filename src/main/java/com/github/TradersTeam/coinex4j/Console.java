07c6a243766f8c3291d89b1a002f383ac449e6c1
Moslem <moslemakhoundi@outlook.com>
2021-10-27 10:22:45 +0330

    change-package-name

--- src/main/java/com/github/TradersTeam/coinex4j/Console.java
+++ src/main/java/com/github/TradersTeam/coinex4j/Console.java
@@ -1,3 +1,5 @@
+package com.github.TradersTeam.coinex4j;
+
 public class Console {
     public static void main(String... args) {
         System.out.println("hello there");
---
+++ src/main/java/com/github/TradersTeam/coinex4j/util/Web.java
@@ -0,0 +1,7 @@
+package com.github.TradersTeam.coinex4j.util;
+
+public class Web {
+    public static void sayHi(){
+        System.out.println("hi");
+    }
+}
