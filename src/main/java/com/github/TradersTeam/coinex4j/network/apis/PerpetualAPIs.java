387ea0ae17afdba9bb7d80e6405f732ce7d63ac7
YaMiN <yamin8000@yahoo.com>
2021-11-06 10:13:38 +0330

    added some docs for PerpetualAPIs

--- src/main/java/com/github/TradersTeam/coinex4j/network/apis/PerpetualAPIs.java
+++ src/main/java/com/github/TradersTeam/coinex4j/network/apis/PerpetualAPIs.java
@@ -15,25 +15,65 @@ public interface PerpetualAPIs {
     String MARKET = "market/";
 
     interface System {
+
+        /**
+         * Test API for checking server status
+         *
+         * @return "pong"
+         */
         @GET(PERPETUAL_V1 + "ping")
         CallX<ApiResponse<String>> getPing();
 
+        /**
+         * Get server time
+         *
+         * @return Server time in milliseconds
+         */
         @GET(PERPETUAL_V1 + "time")
         CallX<ApiResponse<DateTime>> getTime();
     }
 
+    /**
+     * Get perpetual market list
+     *
+     * @return List of perpetual market
+     */
     @GET(PERPETUAL_V1 + MARKET + "list")
     CallX<ApiResponse<List<PerpetualMarket>>> getMarketList();
 
+    /**
+     * Get perpetual market limit config
+     *
+     * @return map of markets and their limit config
+     */
     @GET(PERPETUAL_V1 + MARKET + "limit_config")
     CallX<ApiResponse<Map<String, List<PerpetualLimitConfig>>>> getMarketLimitConfigList();
 
+    /**
+     * Get single market ticker info
+     *
+     * @param market market name
+     * @return market ticker info
+     */
     @GET(PERPETUAL_V1 + MARKET + "ticker")
     CallX<ApiResponse<PerpetualMarketTicker>> getMarketTicker(@Query("market") String market);
 
+    /**
+     * Get all markets ticker info
+     *
+     * @return map of market ticker info and current time
+     */
     @GET(PERPETUAL_V1 + MARKET + "ticker" + "/all")
     CallX<ApiResponse<PerpetualMarketTickers>> getMarketTickers();
 
+    /**
+     * Get market depth info
+     *
+     * @param market market name
+     * @param merge  merge precision, take one of the value among "10", "1", "0", "0.1" and "0.01".
+     * @param limit  the number of entries obtained, take one of the value among 5, 10, 20, 50 and 100.
+     * @return market depth info
+     */
     @GET(PERPETUAL_V1 + MARKET + "depth")
     CallX<ApiResponse<MarketDepth>> getMarketDepth(
             @Query("market") String market, @Query("merge") double merge, @Range(from = 1, to = 50) @Query("limit") int limit
