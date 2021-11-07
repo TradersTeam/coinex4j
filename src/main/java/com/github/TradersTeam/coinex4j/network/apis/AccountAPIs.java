eb547cc3d8edd62e717d26defb44b29e4c36deab
YaMiN <yamin8000@yahoo.com>
2021-11-07 13:25:53 +0330

    added Inquire market fee api

--- src/main/java/com/github/TradersTeam/coinex4j/network/apis/AccountAPIs.java
+++ src/main/java/com/github/TradersTeam/coinex4j/network/apis/AccountAPIs.java
@@ -2,7 +2,9 @@ package com.github.TradersTeam.coinex4j.network.apis;
 
 import com.github.TradersTeam.coinex4j.model.AccountAsset;
 import com.github.TradersTeam.coinex4j.model.ApiResponse;
+import com.github.TradersTeam.coinex4j.model.MarketFee;
 import com.github.TradersTeam.coinex4j.network.util.CallX;
+import org.jetbrains.annotations.Nullable;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
@@ -30,4 +32,33 @@ public interface AccountAPIs {
      */
     @GET(V1 + "margin/market")
     CallX<ApiResponse<Map<String, String>>> getMarginAccountMarketInfo();
+
+    /**
+     * Inquire market fee.
+     *
+     * @param tonce        Required | The timestamp of the request
+     *                     Tonce is a timestamp with a positive Integer that,
+     *                     represents the number of milliseconds from Unix epoch to the current time.
+     *                     Error between tonce and server time can not exceed plus or minus 60s
+     * @param market       Required | The market name
+     * @param businessType Optional | use SPOT or PERPETUAL, default to SPOT
+     * @return market fee
+     */
+    @GET(V1 + "account/market/fee")
+    CallX<ApiResponse<MarketFee>> getMarketFee(
+            @Query("tonce") Long tonce,
+            @Query("market") String market,
+            @Nullable @Query("business_type") String businessType
+    );
+
+    /**
+     * calls {@link #getMarketFee(Long, String, String)},
+     * with business type set to SPOT
+     */
+    default CallX<ApiResponse<MarketFee>> getMarketFee(
+            @Query("tonce") Long tonce,
+            @Query("market") String market
+    ) {
+        return getMarketFee(tonce, market, null);
+    }
 }
